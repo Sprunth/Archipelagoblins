0d730d9b28871e3c1528c375135cd179bc1d631b
dylan wang <dylan.wang@gmail.com>
2025-02-16 23:31:45 -0800

    basic nav mesh working

--- Scripts/Pawn.cs
+++ Scripts/Pawn.cs
@@ -1,15 +1,19 @@
 using Godot;
 using System;
 
-public partial class Pawn : AnimatedSprite2D
+public partial class Pawn : CharacterBody2D
 {
 	[Export]
-	public int Speed { get; set; } = 400;
-	private Vector2 target;
+	public int Speed { get; set; } = 200;
+	private Vector2 target = new Vector2(800, 300);
+
+
+	NavigationAgent2D navAgent;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		navAgent = GetNode<NavigationAgent2D>("navigation/NavigationAgent2D");
 
 	}
 
@@ -30,7 +34,22 @@ public partial class Pawn : AnimatedSprite2D
 
 	public override void _PhysicsProcess(double delta)
 	{
-		this.Position = this.Position.MoveToward(target, Speed * (float)delta);
-		base._PhysicsProcess(delta);
+		//this.Position = this.Position.MoveToward(target, Speed * (float)delta);
+		//base._PhysicsProcess(delta);
+
+
+		var direction = Vector2.Zero;
+
+		direction = navAgent.GetNextPathPosition() - GlobalPosition;
+		direction = direction.Normalized();
+
+		Velocity = Velocity.Lerp(direction * Speed, 2f * (float)delta);
+
+		MoveAndSlide();
+	}
+
+	public void OnNavTimerTimeout()
+	{
+		navAgent.TargetPosition = target;
 	}
 }
